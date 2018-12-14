89a1dab5cd8e3349d1b8cf88294646270584cf16
aa shulpin <aashulpin@gmail.com>
2018-12-14 23:02:50 +0500

    cards display everything

--- app/src/main/java/ru/you11/repasttestproject/main/NearbyFragment.kt
+++ app/src/main/java/ru/you11/repasttestproject/main/NearbyFragment.kt
@@ -1,7 +1,9 @@
 package ru.you11.repasttestproject.main
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -22,8 +24,8 @@ class NearbyFragment: Fragment(), NearbyContract.View {
         val root = inflater.inflate(R.layout.fragment_nearby, container, false)
         with(root) {
             val restaurantResults = ArrayList<Restaurant>()
-            restaurantResults.add(Restaurant(0, "Maecenas 1903", 5.0, "Екатеринбург, ул. Братьев Быковых, 74", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://cookit.zenithapps.ru/images/prt.jpg", "12312", 4))
-            restaurantResults.add(Restaurant(0, "Maecenas 1903", 5.0, "Екатеринбург, ул. Братьев Быковых, 74", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://cookit.zenithapps.ru/images/prt.jpg", "12312", 4))
+            restaurantResults.add(Restaurant(0, "Maecenas 1903", 4.5, "Екатеринбург, ул. Братьев Быковых, 74", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://cookit.zenithapps.ru/images/prt.jpg", "12312", 4))
+            restaurantResults.add(Restaurant(0, "Maecenas 1903", 1.3, "Екатеринбург, ул. Братьев Быковых, 74", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "http://cookit.zenithapps.ru/images/prt.jpg", "12312", 4))
 
             val rv = findViewById<RecyclerView>(R.id.nearby_rv)
             rv.layoutManager = LinearLayoutManager(activity)
@@ -45,8 +47,8 @@ class NearbyFragment: Fragment(), NearbyContract.View {
             val address: TextView = layout.findViewById(R.id.rv_restaurant_address)
             val description: TextView = layout.findViewById(R.id.rv_restaurant_description)
             val tipsButton: Button = layout.findViewById(R.id.rv_restaurant_give_tips_button)
-            val callButton: Button = layout.findViewById(R.id.rv_restaurant_call_button)
-            val favoriteButton: Button = layout.findViewById(R.id.rv_restaurant_favorite_button)
+            val callButton: ImageButton = layout.findViewById(R.id.rv_restaurant_call_button)
+            val favoriteButton: ImageButton = layout.findViewById(R.id.rv_restaurant_favorite_button)
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -54,6 +56,8 @@ class NearbyFragment: Fragment(), NearbyContract.View {
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val context = holder.name.context
+
             val restaurant = results[position]
             Picasso.get()
                 .load(restaurant.photo)
@@ -63,10 +67,51 @@ class NearbyFragment: Fragment(), NearbyContract.View {
             holder.rating.text = restaurant.rating.toString()
             holder.address.text = restaurant.address
             holder.description.text = restaurant.description
+
+            addStarsToLayout(context, holder.starsLayout, Math.round(restaurant.rating).toInt())
         }
 
         override fun getItemCount(): Int {
             return results.size
+        }
+
+        private fun addStarsToLayout(context: Context, starsLayout: LinearLayout, number: Int) {
+            for (i in 1..number) {
+                val star = createFullStar(context)
+                starsLayout.addView(star)
+            }
+
+            for (i in number + 1..5) {
+                val star = createEmptyStar(context)
+                starsLayout.addView(star)
+            }
+        }
+
+        private fun createFullStar(context: Context): ImageView {
+            val star = createStarWithoutDrawable(context)
+            val drawable = ContextCompat.getDrawable(context, R.drawable.baseline_star_black_18)
+            star.setImageDrawable(drawable)
+            //TODO: create purple star
+
+            return star
+        }
+
+        private fun createEmptyStar(context: Context): ImageView {
+            val star = createStarWithoutDrawable(context)
+            val drawable = ContextCompat.getDrawable(context, R.drawable.baseline_star_border_black_18)
+            star.setImageDrawable(drawable)
+            //TODO: create purple star
+
+            return star
+        }
+
+        private fun createStarWithoutDrawable(context: Context): ImageView {
+            val star = ImageView(context)
+            val size = context.resources.getDimensionPixelSize(R.dimen.star_size)
+            val lp = LinearLayout.LayoutParams(size, size)
+            star.layoutParams = lp
+
+            return star
         }
     }
 }
